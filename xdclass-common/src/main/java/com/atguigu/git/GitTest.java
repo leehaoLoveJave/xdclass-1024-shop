6b6ad187627013839857adc6518b408cdf36c50e
李浩 <582317419@qq.com>
2021-10-06 19:01:31 +0800

    first commit

---
+++ xdclass-common/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2021-10-06 18:57
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
