33e494d78a7b3a2804948b0f64b0cc8d317c55e1
李浩 <582317419@qq.com>
2021-10-07 11:24:43 +0800

    项目添加公共包

---
+++ xdclass-user-service/src/main/java/User.java
@@ -0,0 +1,15 @@
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author shkstart
+ * @create 2021-10-07 11:10
+ */
+@Data
+@Slf4j
+public class User {
+    private String name;
+    private String age;
+}
